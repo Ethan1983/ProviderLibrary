8ee605325cd2ae9705e98af05079977565ef0e00
Vairavan Srinivasan <vairav@codeaurora.org>
2019-02-02 20:35:18 -0500

    Changes to JavaLibrary3 and build log

---
+++ JavaLibrary3/src/main/java/com/sample/javalibrary3/Interface3.java
@@ -0,0 +1,8 @@
+package com.sample.javalibrary3;
+
+public interface Interface3 {
+
+    void doSomething();
+
+    void doSomething1();
+}
--- JavaLibrary3/src/main/java/com/sample/javalibrary3/MyClass3.java
+++ JavaLibrary3/src/main/java/com/sample/javalibrary3/MyClass3.java
@@ -1,4 +1,15 @@
 package com.sample.javalibrary3;
 
-public class MyClass3 {
+public class MyClass3 implements Interface3 {
+
+    @Override
+    public void doSomething() {
+
+    }
+
+    @Override
+    public void doSomething1() {
+
+    }
+
 }
